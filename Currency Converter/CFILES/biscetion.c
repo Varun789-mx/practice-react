2e85781b33250d892c800dd5a1e2d3f73bf96946
Harsh <VARUNSH153@GMAIL.COM>
2025-02-05 00:17:16 +0530

    Update biscetion.c
    added small fixed

--- Currency Converter/CFILES/biscetion.c
+++ Currency Converter/CFILES/biscetion.c
@@ -1,31 +1,32 @@
-#include <stdio.h>
-#include <math.h>
+#include<stdio.h>
+#include<math.h>
 
-double fun(double x)
-{
-    return (x * x * x) - (5 * x) + 1;
+double fun(double x) { 
+	return (x*x) - (4*x) - 21;
 }
 
-int main()
-{
-    double x1, x2, x3;
-    int count;
-    printf("Enter the first itteration of [x1 and x2]:");
-    scanf("%lf %lf", &x1, &x2);
-    printf("Enter the number of itterarions : ");
-    scanf("%d", &count);
-    int itteration = 1;
-    while (itteration <= count)
-    {
-        x3 = (x1 + x2) / 2;
-        printf("Itteraton : %d  x1:%.2lf  x2:%lf  x3:%.2lf \n", itteration, x1, x2, x3);
-        if (fun(x3) < 0)
-            x1 = x3;
-        else
-            x2 = x3;
+int main() { 
+	double x1,x2,x3;
+	int itteration;
+	printf("Enter the assumed roots [x1,x2] : ");
+	scanf("%lf %lf",&x1,&x2);
+	printf("\n");
+	printf("Enter the number of itterations: ");
+	scanf("%d",&itteration);
+	int count = 1;
 
-        itteration++;
-    }
-    printf("Approximate root is : %.2lf", x3);
-    return 0;
+	while(count <= itteration) { 
+		x3 = (x1+x2) / 2;
+		printf("Itteration : %d  x1 : %2.lf  x2 : %2.lf x3 : %2.lf \n" , count,x1,x2,x3);
+		if(fun(x3) < 0) { 
+		 x1 = x3;
+		}
+		else { 
+			x2 = x3;
+		}
+		count++;
+	}
+	printf("Approximate root : %2.lf \n",x3); 
+	return 0;
 }
+
